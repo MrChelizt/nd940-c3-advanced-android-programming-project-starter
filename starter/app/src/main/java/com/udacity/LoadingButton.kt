53f3f4481d1400a8e198e41ccedcbf1e772886b9
MrChelizt <chelizt@gmail.com>
2022-10-16 00:56:39 +0200

    Implement Custom button and animate, Download functionality added, Permissions Added

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -1,10 +1,15 @@
 package com.udacity
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Canvas
+import android.graphics.*
+import android.text.TextPaint
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -13,21 +18,138 @@ class LoadingButton @JvmOverloads constructor(
     private var widthSize = 0
     private var heightSize = 0
 
+    private var defaultBackgroundColor = 0
+    private var loadingBackgroundColor = 0
+    private var pacmanColor = 0
+    private var textColor = 0
+    private lateinit var defaultText: String
+    private lateinit var loadingText: String
+    private lateinit var clickedText: String
+
+    private lateinit var currentText: String
+
+    private var loadingBarWidth = 0f
+    private var pacmanAngle = 0f
+
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+    }
+
+    private val textPaint = TextPaint().apply {
+        textAlign = Paint.Align.CENTER
+        textSize = resources.getDimension(R.dimen.default_text_size)
+    }
+
+    private val textHeight = textPaint.descent() - textPaint.ascent()
+    private val textOffset = (textHeight / 2) - textPaint.descent()
+
     private val valueAnimator = ValueAnimator()
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
-
+    var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { property, oldValue, newValue ->
+        when (newValue) {
+            ButtonState.Clicked -> {
+                Log.e("Current clicked", "loading")
+                isClickable = false
+                currentText = clickedText
+                invalidate()
+            }
+            ButtonState.Loading -> {
+                Log.e("Current state", "loading")
+                currentText = loadingText
+                valueAnimator.duration = 5000
+                valueAnimator.setFloatValues(0f, widthSize.toFloat())
+                valueAnimator.addUpdateListener { animator ->
+                    loadingBarWidth = animator.animatedValue as Float
+                    pacmanAngle = loadingBarWidth * 360 / widthSize
+                    invalidate()
+                }
+                valueAnimator.addListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        if (buttonState == ButtonState.Loading) {
+                            valueAnimator.start()
+                        }
+                    }
+                })
+                valueAnimator.start()
+            }
+            ButtonState.Completed -> {
+                Log.e("Current state", "completed")
+                valueAnimator.cancel()
+                isClickable = true
+                currentText = defaultText
+                loadingBarWidth = 0f
+                pacmanAngle = 0f
+                invalidate()
+            }
+        }
     }
 
 
     init {
-
+        isClickable = true
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            defaultBackgroundColor =
+                getColor(R.styleable.LoadingButton_defaultBackgroundColor, Color.RED)
+            loadingBackgroundColor =
+                getColor(R.styleable.LoadingButton_loadingBackgroundColor, Color.BLUE)
+            pacmanColor = getColor(R.styleable.LoadingButton_pacmanColor, Color.YELLOW)
+            textColor = getColor(R.styleable.LoadingButton_textColor, Color.BLUE)
+            defaultText = getString(R.styleable.LoadingButton_defaultText).toString()
+            loadingText = getString(R.styleable.LoadingButton_loadingText).toString()
+            clickedText = getString(R.styleable.LoadingButton_clickedText).toString()
+            currentText = defaultText
+        }
     }
 
-
-    override fun onDraw(canvas: Canvas?) {
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        drawButton(canvas)
+        drawLoadingBar(canvas)
+        drawButtonText(canvas)
+        drawPacman(canvas)
+    }
 
+    private fun drawLoadingBar(canvas: Canvas) {
+        canvas.save()
+        val progressBar = RectF(
+            0f, 0f, loadingBarWidth, measuredHeight.toFloat()
+        )
+        paint.color = loadingBackgroundColor
+        canvas.drawRect(progressBar, paint)
+    }
+
+    // even though drawCircle is better suited name here drawPacman is more fun
+    private fun drawPacman(canvas: Canvas) {
+        canvas.save()
+        val pacmanRect = RectF(
+            3 * measuredWidth.toFloat() / 4 - textOffset * 2,
+            measuredHeight.toFloat() / 4 + textOffset / 2,
+            3 * measuredWidth.toFloat() / 4 + textHeight - textOffset * 2,
+            measuredHeight.toFloat() / 4 + textHeight + textOffset / 2
+        )
+        paint.color = pacmanColor
+        canvas.drawArc(
+            pacmanRect, 0f, pacmanAngle, true, paint
+        )
+    }
+
+    private fun drawButton(canvas: Canvas) {
+        canvas.save()
+        val downloadButtonRect = RectF(
+            0f, 0f, measuredWidth.toFloat(), measuredHeight.toFloat()
+        )
+        paint.color = defaultBackgroundColor
+        canvas.drawRect(downloadButtonRect, paint)
+    }
+
+    private fun drawButtonText(canvas: Canvas) {
+        canvas.save()
+        textPaint.color = textColor
+        canvas.drawText(
+            currentText,
+            measuredWidth.toFloat() / 2, measuredHeight.toFloat() / 2 + textOffset, textPaint
+        )
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
--- starter/app/src/main/java/com/udacity/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.udacity
 
+import android.Manifest
 import android.app.DownloadManager
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -7,10 +8,19 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.content.pm.PermissionInfo
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -30,22 +40,96 @@ class MainActivity : AppCompatActivity() {
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
+        val radioGroup: RadioGroup = findViewById(R.id.radioGroup)
+
         custom_button.setOnClickListener {
-            download()
+            custom_button.buttonState = ButtonState.Clicked
+            when (radioGroup.checkedRadioButtonId) {
+                R.id.glideButton -> download(GLIDE_URL)
+                R.id.loadAppButton -> download(LOAD_APP_URL)
+                R.id.retrofitButton -> download(RETROFIT_URL)
+                else -> {
+                    Toast.makeText(
+                        baseContext,
+                        getString(R.string.select_project_warning),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    custom_button.buttonState = ButtonState.Completed
+                }
+            }
         }
     }
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            val query = DownloadManager.Query()
+            query.setFilterById(id?:downloadID)
+            val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            val cursor = downloadManager.query(query)
+            if (cursor.moveToFirst()) {
+                if (cursor.count > 0) {
+                    val statusI = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)
+                    when (cursor.getInt(statusI)) {
+                        DownloadManager.STATUS_SUCCESSFUL -> {
+                            Log.e("Download status", "success")
+                            custom_button.buttonState =
+                                ButtonState.Completed
+                        }
+                        DownloadManager.STATUS_FAILED -> {
+                            Log.e("Download status", "fail")
+                            custom_button.buttonState =
+                                ButtonState.Completed
+                        }
+                    }
+                }
+            }
         }
     }
 
-    private fun download() {
+    private fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val nw = connectivityManager.activeNetwork ?: return false
+        val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+        return when {
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            //for other device how are able to connect with Ethernet
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            //for check internet over Bluetooth
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+            else -> false
+        }
+    }
+
+    private fun checkPermissions(): Boolean {
+        if (isNetworkAvailable(this)) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                return true
+            } else {
+                requestPermissions(
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    PermissionInfo.PROTECTION_DANGEROUS
+                )
+            }
+        }
+        custom_button.buttonState = ButtonState.Completed
+        return false
+    }
+
+    private fun download(url: String) {
+        if (!checkPermissions()) return
+        custom_button.buttonState = ButtonState.Loading
         val request =
-            DownloadManager.Request(Uri.parse(URL))
+            DownloadManager.Request(Uri.parse(url))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
+                .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "/project.zip")
                 .setRequiresCharging(false)
                 .setAllowedOverMetered(true)
                 .setAllowedOverRoaming(true)
@@ -56,8 +140,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        private const val URL =
+        private const val GLIDE_URL = "https://github.com/bumptech/glide/archive/master.zip"
+        private const val LOAD_APP_URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val RETROFIT_URL = "https://github.com/square/retrofit/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
     }
 
